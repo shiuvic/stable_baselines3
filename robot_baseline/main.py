e30ab17d53efd78a9c5a135547a1bfa05c22e5b8
shiuv <cbc107023@nptu.edu.tw>
2022-06-15 17:37:05 +0800

    update to stable baseline3

--- robot_baseline/main.py
+++ robot_baseline/main.py
@@ -1,10 +1,4 @@
 import gym
-# from stable_baselines3.common.env_util import make_vec_env
-# from stable_baselines3 import PPO
-# from stable_baselines.common.callbacks import CheckpointCallback
-# multiprocess environment
-# checkpoint_callback = CheckpointCallback(save_freq=100000, save_path='./logs/',
-#                                          name_prefix='rl_model')
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
